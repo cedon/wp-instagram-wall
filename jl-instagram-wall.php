002c370e431ff2240bbbf7b5906a491a34a5a3e6
Jon Liebold <jliebold@outlook.com>
2017-12-05 21:20:09 -0500

    Added metadata to plugin file

--- jl-instagram-wall.php
+++ jl-instagram-wall.php
@@ -2,10 +2,10 @@
 
 /*
 Plugin Name: Jl Instagram Wall
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A brief description of the Plugin.
+Plugin URI: https://www.jonliebold.com/
+Description: A simple concept plugin for WordPress that creates combined Instagram searches via the API and creates a "wall" for them in the form of a custom post type.
 Version: 1.0
 Author: Jon
-Author URI: http://URI_Of_The_Plugin_Author
-License: A "Slug" license name e.g. GPL2
+Author URI: https://www.jonliebold.com/
+License: GPL2
 */
